7a1f7835f0a3e54bcd9e78b52f61d6c62fd1fa10
Brad Barrows <51488851+bebrws@users.noreply.github.com>
2020-08-05 11:38:46 -0700

    swift 5

--- MaterialPalette/SwiftPriorityQueue.swift
+++ MaterialPalette/SwiftPriorityQueue.swift
@@ -42,7 +42,7 @@ public struct PriorityQueue<T: Comparable> {
             ordered = { $0 < $1 }
         }
         
-        for value in startingValues { push(value) }
+        for value in startingValues { push(element: value) }
     }
     
     /// How many elements the Priority Queue stores
@@ -56,7 +56,8 @@ public struct PriorityQueue<T: Comparable> {
     /// - parameter element: The element to be inserted into the Priority Queue.
     public mutating func push(element: T) {
         heap.append(element)
-        swim(heap.count - 1)
+        var heapMinsOne = heap.count - 1
+        swim(index: &heapMinsOne)
     }
     
     /// Remove and return the element with the highest priority (or lowest if ascending). O(lg n)
@@ -67,9 +68,10 @@ public struct PriorityQueue<T: Comparable> {
         if heap.isEmpty { return nil }
         if heap.count == 1 { return heap.removeFirst() }  // added for Swift 2 compatibility
         // so as not to call swap() with two instances of the same location
-        swap(&heap[0], &heap[heap.count - 1])
+        heap.swapAt(0, heap.count - 1)
         let temp = heap.removeLast()
-        sink(0)
+        var index = 0
+        sink(index: &index)
         
         return temp
     }
@@ -83,50 +85,54 @@ public struct PriorityQueue<T: Comparable> {
     
     /// Eliminate all of the elements from the Priority Queue.
     public mutating func clear() {
-        heap.removeAll(keepCapacity: false)
+        heap.removeAll(keepingCapacity: false)
     }
     
     // Based on example from Sedgewick p 316
-    private mutating func sink(var index: Int) {
+    private mutating func sink(index: inout Int) {
         
         while 2 * index + 1 < heap.count {
             
             var j = 2 * index + 1
             
-            if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j++ }
+            if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j+=1 }
             if !ordered(heap[index], heap[j]) { break }
             
-            swap(&heap[index], &heap[j])
+            heap.swapAt(index, j)
             index = j
         }
     }
     
     // Based on example from Sedgewick p 316
-    private mutating func swim(var index: Int) {
+    private mutating func swim(index: inout Int) {
         
         while index > 0 && ordered(heap[(index - 1) / 2], heap[index]) {
-            swap(&heap[(index - 1) / 2], &heap[index])
+            heap.swapAt((index - 1) / 2, index)
             index = (index - 1) / 2
         }
     }
 }
 
 // MARK: - GeneratorType
-extension PriorityQueue: GeneratorType {
+extension PriorityQueue: IteratorProtocol {
     
     public typealias Element = T
     mutating public func next() -> Element? { return pop() }
 }
 
 // MARK: - SequenceType
-extension PriorityQueue: SequenceType {
+extension PriorityQueue: Sequence {
     
     public typealias Generator = PriorityQueue
     public func generate() -> Generator { return self }
 }
 
 // MARK: - CollectionType
-extension PriorityQueue: CollectionType {
+extension PriorityQueue: Collection {
+    public func index(after i: Int) -> Int {
+        return i + 1
+    }
+    
     
     public typealias Index = Int
     
